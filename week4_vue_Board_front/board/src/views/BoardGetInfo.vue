77eed11ded39465bdc8f6c66e96484be3c54baf8
jeonje <whssodi@gmail.com>
2023-06-15 04:09:49 +0900

    feat : week4 view(댓글 등록)

--- week4_vue_Board_front/board/src/views/BoardGetInfo.vue
+++ week4_vue_Board_front/board/src/views/BoardGetInfo.vue
@@ -33,7 +33,7 @@
                     </div>
                 </div>
                 <div class="d-flex justify-content-between pt-2">
-                      <h5 class="card-title mb-4">{{ this.board.categoryName }}: {{ board.title }}</h5>
+                    <h5 class="card-title mb-4">{{ this.board.categoryName }}: {{ board.title }}</h5>
                     <p class="card-text mb-4">조회수: {{ updatedVisitCount }}</p>
                 </div>
             </div>
@@ -62,7 +62,7 @@
         </div>
         <!-- 댓글 추가 -->
         <div class="d-flex justify-content-center my-3">
-            <form @submit="addComment" class="w-75">
+            <form @submit.prevent="addComment" class="w-75">
                 <div class="row">
                     <div class="col-8">
                         <div class="form-group">
@@ -71,7 +71,7 @@
                         </div>
                     </div>
                     <div class="col-4 d-flex align-items-center">
-                        <button class="btn btn-primary" type="submit">댓글 등록</button>
+                        <button type="submit" class="btn btn-primary">댓글 등록</button>
                     </div>
                 </div>
             </form>
@@ -85,7 +85,7 @@
                 <button class="btn btn-primary" @click="handlePasswordModal('delete')">삭제</button>
             </div>
         </div>
-</div>
+    </div>
 </template>
 
 <script>
@@ -101,18 +101,18 @@ export default {
             passwordError: '',
             board: {
                 boardId: '',
-                writer: '', 
+                writer: '',
                 createdAt: '',
-                modifiedAt: '', 
-                categoryId: '', 
-                title: '', 
-                content: '', 
+                modifiedAt: '',
+                categoryId: '',
+                title: '',
+                content: '',
                 categoryName: '',
             },
-            updatedVisitCount: 0, 
-            attachments: [], 
-            comments: [], 
-            commentContent: '', 
+            updatedVisitCount: 0,
+            attachments: [],
+            comments: [],
+            commentContent: '',
             searchCondition: {
                 currentPage: '',
                 categoryId: '',
@@ -129,13 +129,14 @@ export default {
         this.searchCondition.startDate = this.$route.query.startDate;
         this.searchCondition.endDate = this.$route.query.endDate;
         this.board.boardId = this.$route.query.boardId;
-        
+
         // 게시글 정보를 가져오는 API 호출
         this.fetchBoardData();
-        
+
     },
     methods: {
         fetchBoardData() {
+            console.log("fetchBaord ")
             // 게시글 정보를 가져오는 API 호출 및 데이터 할당
             const url = `board/view?boardId=${this.board.boardId}&categoryId=${this.searchCondition.categoryId}&searchText=${this.searchCondition.searchText}&startDate=${this.searchCondition.startDate}&endDate=${this.searchCondition.endDate}&currentPage=${this.searchCondition.currentPage}`;
             api
@@ -143,20 +144,18 @@ export default {
                 .then(response => {
                     const responseData = response.data.data;
                     Object.assign(this.board, responseData.board);
+                    Object.assign(this.comments, responseData.comments);
                 })
                 .catch(error => {
                     console.log(error);
                 })
-   
+
         },
         getCategoryName(categoryId) {
             // 카테고리 ID를 기반으로 카테고리 이름을 가져오는 로직
-            // 예시:
             return this.categories.find((category) => category.id === categoryId)?.name || '';
         },
         dateFormat(date) {
-            // 날짜 포맷 변환 로직
-            // 예시:
             return moment(date).format('YYYY-MM-DD');
         },
         hidePasswordModal() {
@@ -171,46 +170,47 @@ export default {
             const hashedPassword = this.$CryptoJS.SHA256(this.enteredPassword).toString();
             // 비밀번호 해싱 로직 (예: CryptoJS.SHA256(enteredPassword).toString())
 
-      // 비밀번호 검증 API 호출
-      // 호출 결과에 따라 처리
+            // 비밀번호 검증 API 호출
+            // 호출 결과에 따라 처리
 
-        if (hashedPassword === this.password) {
-                    this.hidePasswordModal();
-                    if (this.isEdit) {
-                        // 수정 페이지로 이동하는 로직
-                        // 예시:
-                        // const redirectURL = `modify?action=modify&id=${this.board.boardId}&page=${this.param.page}&category=${this.param.category}&search=${this.param.searchText}&startDate=${this.param.startDate}&endDate=${this.param.endDate}`;
-                        // window.location.replace(redirectURL);
-                    } else {
-                        try {
-                            // 삭제 API 호출
-                            // 호출 결과에 따라 처리
-                            // 예시:
-                            // const response = await axios.post('/api/board/delete', {
-                            //   id: this.board.boardId,
-                            //   password: enteredPassword,
-                            // });
-                            // if (!response.ok) {
-                            //   alert('삭제에 실패하였습니다');
-                            // }
-                            // window.location.replace('list');
-                        } catch (error) {
-                            alert(error);
-                        }
-                    }
+            if (hashedPassword === this.password) {
+                this.hidePasswordModal();
+                if (this.isEdit) {
+
+                    //TODO 
                 } else {
-                    this.passwordError = '비밀번호가 일치하지 않습니다.';
+                    try {
+
+                        //TODO 
+                    } catch (error) {
+                        alert(error);
+                    }
                 }
+            } else {
+                this.passwordError = '비밀번호가 일치하지 않습니다.';
+            }
         },
         addComment() {
             // 새로운 댓글 등록 로직
-            // API 호출 예시: axios.post('/api/comment/add', { content: this.commentContent, boardId: this.board.boardId })
-            // 호출 결과에 따라 처리
-            // 댓글 등록 후, 새로운 댓글 목록을 가져와서 this.comments에 할당
-            // 예시:
-            // this.commentContent = '';
-            // const response = await axios.post('/api/comment/list', { boardId: this.board.boardId });
-            // this.comments = response.data.comments;
+            const requestBody = {
+                content: this.commentContent,
+                boardId: this.board.boardId
+            };
+
+            const url = `board/add-comment`
+            api
+                .post(url, requestBody)
+                .then(response => {
+                    const responseData = response.data.data;
+
+                    this.comments = responseData.comments;
+                    this.commentContent = '';
+
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+
         },
     },
 };
